ad454a6388a6f05fab1ae9e147f6d2fee18c7080
john <misonjohnemmanuel@gmail.com>
2025-12-04 23:35:33 +0800

    JWT middleware adjustments

--- routes/api.php
+++ routes/api.php
@@ -5,13 +5,9 @@ use App\Http\Controllers\EmployeeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:api')->group(function () {
+Route::middleware('jwt.auth')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::apiResource('employees', EmployeeController::class);
