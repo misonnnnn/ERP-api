f942c305663ad043e2386811c7718e90ce3cc900
john <misonjohnemmanuel@gmail.com>
2025-12-09 22:57:24 +0800

    command via url

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/run-artisan/{command}', function ($command) {
+    // Security: allow only specific commands
+    $allowed = [
+        'migrate',
+        'config:cache',
+        'route:clear',
+        'view:cache',
+        'storage:link',
+        'optimize',
+    ];
+
+    if (! in_array($command, $allowed)) {
+        return "Command not allowed.";
+    }
+
+    \Illuminate\Support\Facades\Artisan::call($command);
+
+    return "<pre>" . \Illuminate\Support\Facades\Artisan::output() . "</pre>";
+});
